c0a97bba155a8c498cf4ebdcc4606f4fa581be73
Kade Robertson <kade@kaderobertson.dev>
2023-07-31 13:21:10 -0400

    chore: initial app config

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use tracing::*;
+use tracing_subscriber::prelude::*;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let logger = tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new("INFO"))
+        .with(tracing_subscriber::fmt::layer());
+    logger.init();
+
+    info!("Hello, world!");
+
+    Ok(())
+}
