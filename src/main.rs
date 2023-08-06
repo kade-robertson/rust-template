ad30ff1f6cf115a2ccc3df65cbeb6b30eb1bbf04
Kade Robertson <kade@kaderobertson.dev>
2023-08-06 12:39:53 -0400

    fix: improved default configuration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+use serde::Deserialize;
+use std::io::{stdin, IsTerminal};
 use tracing::*;
 use tracing_subscriber::prelude::*;
 
+fn json_log_default() -> bool {
+    !stdin().is_terminal()
+}
+
+#[derive(Deserialize, Debug)]
+struct Environment {
+    #[serde(default = "json_log_default")]
+    json_log: bool,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let logger = tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new("INFO"))
-        .with(tracing_subscriber::fmt::layer());
-    logger.init();
+    let _ = dotenvy::dotenv();
+    let env = envy::from_env::<Environment>()?;
+
+    let logger = tracing_subscriber::registry().with(tracing_subscriber::EnvFilter::new("INFO"));
+    if env.json_log {
+        logger.with(tracing_subscriber::fmt::layer().json()).init();
+    } else {
+        logger.with(tracing_subscriber::fmt::layer()).init();
+    }
 
     info!("Hello, world, from {}!", env!("CARGO_PKG_NAME"));
 
